c20bfc237accbe6f335f0a2fd78edb64e72c7361
gomar <tb_vdb@hotmail.com>
2024-09-28 00:08:20 +0200

    added flakes

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  description = "GomNix multi machine magic flake";
+
+  inputs = {
+    
+    ##########################################################
+    # NixOS and HomeManager package sources
+    ##########################################################
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05;
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+    
+    ##########################################################
+    # Utilities
+    ##########################################################
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Stylix input for additional configurations
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ##########################################################
+    # Secrets Management
+    ##########################################################
+   
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ##########################################################
+    # Hyprland when needed
+    ##########################################################
+       
+    hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+#    hyprland-plugins = {
+#      url = "github:hyprwm/hyprland-plugins";
+#      inputs.hyprland.follows = "hyprland";
+#    };
+
+    ##########################################################
+    # Personal Secrets Repo
+    ##########################################################
+ 
+    # nix-secrets = {
+    #   url = "git+ssh://git@gitlab.com/emergentmind/nix-secrets.git?ref=main&shallow=1";
+    #   inputs = { };
+    # };
+
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+
+      # EOS - Home Desktop configuration
+      eos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/eos/configuration.nix # Home desktop VM's configuration
+          inputs.home-manager.nixosModules.default
+          inputs.stylix.nixosModules.stylix
+        ];
+      };
+
+      # FREEDMAN - Laptop configuration
+      freedman = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/freedman/configuration.nix # Home desktop VM's configuration
+          inputs.home-manager.nixosModules.default
+          inputs.stylix.nixosModules.stylix
+        ];
+      };
+
+
+    };
+  };
+}
+
+
+
+
+
+
+
+
+
