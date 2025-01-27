ef8ac66a266ce6ac6497a63a62fbe04f34e200a2
gomar <tb_vdb@hotmail.com>
2025-01-27 09:49:50 +0100

    flakeupdate

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     }; 
     
