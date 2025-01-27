de483eb18bd7f082ac7e85bd433aa0b9826cfb81
gomar <tb_vdb@hotmail.com>
2025-01-27 09:46:57 +0100

    flake updates

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     ##########################################################
 
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
